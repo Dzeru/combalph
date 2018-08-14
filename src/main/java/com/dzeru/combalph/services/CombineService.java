82c724fd888ff6c2cde8a313520414092bd8bf86
Dzeru <diana.mashkina.00@gmail.com>
2018-08-14 08:02:35 +0400

    add license to source files

--- src/main/java/com/dzeru/combalph/services/CombineService.java
+++ src/main/java/com/dzeru/combalph/services/CombineService.java
@@ -8,6 +8,22 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
 
+/**
+   Copyright 2018, Dzeru, Combalph
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
 /*
 Service for combining two alphabets: en or ru to hiragana or katakana.
 Returns text with kana.
@@ -21,8 +37,8 @@ public class CombineService
 
 	public String combine(String text, String language, String kana, String complexityLevel) throws IOException
 	{
-		/*
-		Enum with int param. @See com.dzeru.combalph.objects.ComplexityLevel class for more info.
+		/**
+		Enum with int param. @see com.dzeru.combalph.objects.ComplexityLevel class for more info.
 		 */
 		ComplexityLevel complexitylvl = ComplexityLevel.valueOf(complexityLevel);
 		int complvl = complexitylvl.getComplvl();
@@ -56,9 +72,9 @@ public class CombineService
 				c1 = Character.toString(text.charAt(i + 1));
 			}
 
-			/*
-			Are used for ru-en translation, if @Param language == "ru"
-			If @Param language == "en", e0 and e1 equal c0 and c1
+			/**
+			Are used for ru-en translation, if @param language == "ru"
+			If @param language == "en", e0 and e1 equal c0 and c1
 			Are used in getProperty()
 			 */
 			String e0 = c0;
